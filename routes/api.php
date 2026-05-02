0487b2f62301a0a45c6b002f66a27b716ae461c1
Fadly <radityaeddye@gmail.com>
2026-05-02 14:14:57 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/cek', function () {
+    return ["status" => "Aman!", "pesan" => "Web API Inventory sudah aktif"];
+});
