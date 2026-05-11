9d4b6ecfeb62a88d0be92567b8f4cd613770df1f
Fadly <radityaeddye@gmail.com>
2026-05-11 18:49:57 +0800

    Laporan 2

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +12,6 @@ Route::get('/user', function (Request $request) {
 Route::get('/cek', function () {
     return ["status" => "Aman!", "pesan" => "Web API Inventory sudah aktif"];
 });
+
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('items', ItemController::class);
