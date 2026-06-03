d93ab517c5f430b7b834ea51d0c6e03891b570c1
Fadly <radityaeddye@gmail.com>
2026-06-03 15:34:59 +0800

    Laporan 4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Tambahkan ini 👇
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
 
